d213c3e3de2d31a4125e2b42f5dc3a6060129178
zarman imran <Zarman-oss>
2023-08-10 23:08:22 +0500

    added buttons animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
+import { useState } from 'react';
+
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <>
       <div className="div">
-        <h1>Count:</h1>
+        <h1>Count:{count}</h1>
       </div>
       <div className="btn-container">
-        <button>Increment</button>
-        <button>Decrement</button>
+        <button
+          onClick={() => {
+            setCount(count + 1);
+          }}
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => {
+            setCount(count - 1);
+          }}
+        >
+          Decrement
+        </button>
       </div>
     </>
   );
