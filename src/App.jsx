db29db2327917fbcea6fe5d8beb82fe8d1e2a1eb
zarman imran <Zarman-oss>
2023-08-18 00:30:24 +0500

    changed useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import { useState } from 'react';
-=======
 // import { useState } from 'react';
-  // Branch feature
+// Branch feature
 // import Data from './Data';
 import UserList from './layout/UserList';
 import { UserProvider } from './context/UserContext';
-
 
 function App() {
   // const [user, userData] = useState(Data);
