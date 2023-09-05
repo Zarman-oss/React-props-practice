d002f7551587222aae40bdd994ca1dbe69c12201
zarman imran <zarmanimran@gmail.com>
2023-09-05 13:00:15 +0500

    commit it

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,45 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useReducer } from 'react';
 // Branch feature
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'increment':
+      return { count: state.count + 1 };
+    case 'decrement':
+      return { count: state.count - 1 };
+    default:
+      throw new Error();
+  }
+};
 function App() {
-  const [count, setCount] = useState(0);
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
+  // const [count, setCount] = useState(0);
   const [isEven, setisEven] = useState(true);
   useEffect(() => {
-    if (count % 2 == 0) {
+    if (state.count % 2 == 0) {
       setisEven(true);
     } else {
       setisEven(false);
     }
-  }, [count]);
+  }, [state.count]);
 
   return (
     <div>
       <div className="div">
-        <h1>Count : {count}</h1>
+        <h1>Count : {state.count}</h1>
         <h2>{'Even number: ' + isEven} </h2>
       </div>
       <div className="btn-container">
         <button
           onClick={() => {
-            setCount(count + 1);
+            dispatch({ type: 'increment' });
           }}
         >
           Increment
         </button>
         <button
           onClick={() => {
-            setCount(count - 1);
+            dispatch({ type: 'decrement' });
           }}
         >
           Decrement
