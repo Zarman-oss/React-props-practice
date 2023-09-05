b3ea48842b9aaee3325b873f20d4f1d7f0c30fa4
zarman imran <zarmanimran@gmail.com>
2023-09-05 13:34:58 +0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,35 @@
-import { useState, useEffect, useReducer } from 'react';
+import { useEffect, useReducer } from 'react';
 // Branch feature
 
 const reducer = (state, action) => {
   switch (action.type) {
     case 'increment':
-      return { count: state.count + 1 };
+      return { ...state, count: state.count + 1 };
     case 'decrement':
-      return { count: state.count - 1 };
+      return { ...state, count: state.count - 1 };
+    case 'setEven':
+      return {
+        ...state,
+        isEven: action.payload,
+      };
     default:
       throw new Error();
   }
 };
 function App() {
-  const [state, dispatch] = useReducer(reducer, { count: 0 });
+  const [state, dispatch] = useReducer(reducer, { count: 0, isEven: 'false' });
   // const [count, setCount] = useState(0);
-  const [isEven, setisEven] = useState(true);
+  // const [isEven, setisEven] = useState(true);
   useEffect(() => {
-    if (state.count % 2 == 0) {
-      setisEven(true);
-    } else {
-      setisEven(false);
-    }
+    const isCountEven = state.count % 2 === 0;
+    dispatch({ type: 'setEven', payload: isCountEven });
   }, [state.count]);
 
   return (
     <div>
       <div className="div">
         <h1>Count : {state.count}</h1>
-        <h2>{'Even number: ' + isEven} </h2>
+        <h2>{'Even number: ' + state.isEven} </h2>
       </div>
       <div className="btn-container">
         <button
